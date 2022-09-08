d9af915cc8815c4b9dcb0649097b2d10962eae52
massimiliano <maxvilla970@gmail.com>
2022-09-08 17:17:52 +0200

    Fix pre-processing

--- preprocessing/indexing.py
+++ preprocessing/indexing.py
@@ -1,12 +1,13 @@
 import os
 import sys
 from whoosh.index import create_in, open_dir
+from whoosh.qparser import QueryParser
 from whoosh.fields import *
 
 
 srcFolder = '/home/massimiliano/UNI/Gestione_dell_informazione/thematic-search-engine/doc_tokens/'
 files = os.listdir(srcFolder)
-
+print(files)
 
 if not files:
     sys.exit("\'doc_tokens\' dir is empty cannot works")
@@ -36,3 +37,20 @@ for f_name in files:
     writer.add_document(title=f_name[:-4], content=tkns)
 
 writer.commit()
+"""
+ix = open_dir("indexdir")
+searcher = ix.searcher()
+# print(list(searcher.lexicon("content")))
+parser = QueryParser("content", schema=ix.schema)
+query = parser.parse(u"engineering")
+results = searcher.search(query)
+if len(results) == 0:
+    print("Empty result!!")
+else:
+    for x in results:
+        print(x)
+"""
+
+
+
+
--- preprocessing/preprocessing.py
+++ preprocessing/preprocessing.py
@@ -5,11 +5,21 @@ import sys
 import pdftotext
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
+from nltk.stem.lancaster import LancasterStemmer
 from krovetzstemmer import Stemmer
 from contextlib import redirect_stdout
 
 
-srcFolder = '/home/massimiliano/UNI/Gestione_dell_informazione/thematic-search-engine/documents/'
+"""
+fare un ciclo in piu: il primo pesca da scrFolder + "open-textbooks" il secodo pesca da scrFolder + "springer"
+ovviamente andrà cambiata dstFolder nelle altre due nuove cartelle cambiati.
+Modificare indexdir in modo da aggiunger un altro indice.
+indexdir/open-textbook/....
+        /springer/.... 
+"""
+
+srcFolder = '/home/massimiliano/UNI/Gestione_dell_informazione/thematic-search-engine/documents/open-textbooks/'
+
 dstFolder = '/home/massimiliano/UNI/Gestione_dell_informazione/thematic-search-engine/doc_tokens/'
 
 files = os.listdir(srcFolder)
@@ -18,25 +28,19 @@ if not files:
     sys.exit("\'documents\' dir is empty cannot works")
 
 
-# servirà anche nella query sulla gui
-def tokenize(text):
-    tkn = word_tokenize(text)
-    return tkn
-
-
 for f_name in files:
     raw_f_path = srcFolder + f_name
-
     if f_name[-3:] == "pdf":
         # Load your PDF
         try:
             with open(raw_f_path, "rb") as f:
-                pdf = pdftotext.PDF(f)
+                txt = pdftotext.PDF(f)
         except FileNotFoundError:
             print('File \'{0}\' does not exit.'.format(f_name))
 
         # TOKENIZATION FOR PDF
-        tokens = tokenize("\n\n".join(pdf))
+        tokens = word_tokenize("\n\n".join(txt))
+        # print(tokens)
     else:
         try:
             with open(raw_f_path, 'r') as f:
@@ -55,20 +59,26 @@ for f_name in files:
     # STOPWORDS REMOVAL & LEMMATIZATION
     filt_words = [wnl.lemmatize(tkn.lower()) for tkn in tokens if tkn.lower() not in stops]
 
+
     # STEMMING
     # krovetzstemmer meno aggressivo di Porter
+
+    """
     stemmer = Stemmer()
     stem_words = []
     for word in filt_words:
-        stem_words.append(stemmer.stem(word))
+        stem_words.append(stemmer.stem(word))"""
+
+    lancaster = LancasterStemmer()
+    for word in filt_words:
+        word = lancaster.stem(word)
 
     # REMOVE PUNCTUATION
-    punc = set(string.punctuation + "“" + "”" + "©" + "’")
+    punc = set(string.punctuation + "“" + "”" + "©" + "’" + "∞")
 
-    for word in stem_words:
+    for word in filt_words:
         if word in punc:
-            print("Ho rimosso: ", word)
-            stem_words.remove(word)
+            filt_words.remove(word)
 
     file_tokens_path = dstFolder + f_name[:-4] + "_tokens.txt"
 
@@ -78,6 +88,6 @@ for f_name in files:
     # stampa i tokens su file in "doc_tokens"
     tokens_file = open(file_tokens_path, 'w')
     with redirect_stdout(tokens_file):
-        print(stem_words)
+        print(filt_words)
     tokens_file.close()
     
