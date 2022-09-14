fdcf1c283152bdab039914713ff66118e4780c82
243485 <243485@studenti.unimore.it>
2022-09-14 18:32:56 +0200

    benchmarking

--- src/queryprocessing.py
+++ src/queryprocessing.py
@@ -68,45 +68,5 @@ def search_something():
         if corrected.query != q:
             print("\n ----> Maybe did you mean:", corrected.string + " ?")
 
-
-"""
-def benchmarking():
-    tot_q = 10
-    # ragionarci
-    precisions = [1, 0.948, 1, 0.975, 0.218, 1, 1, 1, 1, 1]
-    map_val = sum(precisions)/tot_q
-
-    natural_queries = []
-    with open(QUERY_NATURAL_PATH, 'r') as f:
-        for line in f:
-            # print(line, "-----")
-            natural_queries.append(line[:-1])
-
-    comp_queries = []
-    with open(QUERY_BENCH_PATH, 'r') as f:
-        for line in f:
-            # print(line, "-----")
-            comp_queries.append(line[:-1])
-
-    final_res = []
-    for cq in comp_queries:
-        x = search_something(cq)[:11]
-        final_res.append(x)
-
-    # print(final_res)
-    for fs in final_res:
-
-        print(f'Natural query: { natural_queries.pop(0) }')
-        print(f'Executed query: { comp_queries.pop(0) }')
-    
-        print('Results number: ' + str(len(fs)))
-        print(fs)
-        break
-        print(f'Average Precision for first 10 results: { precisions.pop(0) } \n\n')
-    print(f'\nMean Average Precision: { map_val } \n')
-
-benchmarking()
-"""
-
 if __name__ == '__main__':
     search_something()
