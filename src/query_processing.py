f4419eabb1ddb412576221b370ead2b76f833ed7
stefanoghinelli <stefano.ghinelli@yahoo.com>
2024-01-14 10:24:40 +0100

    feat: refactored query processing

--- src/query_processing.py
+++ src/query_processing.py
@@ -2,29 +2,25 @@ from whoosh.qparser import MultifieldParser, OrGroup, AndGroup
 from whoosh import scoring
 from whoosh import index
 
-OPENB_INDEX_PATH = "../indexdir/open-textbooks"
-SPR_INDEX_PATH = "../indexdir/springer"
-
-QUERY_NATURAL_PATH = "../evaluation/query_natural_lang.txt"
-QUERY_BENCH_PATH = "../evaluation/query_benchmark.txt"
-
+INDEX_PATH = "../data/indexes"
 
 def submit_query(user_query, idx_path):
     ix = index.open_dir(idx_path)
-    qp = MultifieldParser(["title",
+    qp = MultifieldParser(["title", 
+                           "abstract",
                            "content"],  # all selected fields
                           schema=ix.schema,  # my schema
                           group=OrGroup  # OR instead AND
                           )
     if "AND" in user_query:
         qp = MultifieldParser(["title",
+                               "abstract",
                                "content"],  # all selected fields
                               schema=ix.schema,  # my schema
                               group=AndGroup  # AND instead OR
                               )
 
     user_query = user_query.lower()
-
     # print("This is uin: " + user_query)
     q = qp.parse(user_query)
 
@@ -32,29 +28,23 @@ def submit_query(user_query, idx_path):
     with ix.searcher(weighting=scoring.BM25F(B=0.75, content_B=1.0, K1=1.2)) as searcher:
         results = searcher.search(q, limit=5)
         ret = []
-
         # print(str(len(results)) + " results\n")
         for r in results:
             # print(r.rank, ".", r["title"], " (Score: ", r.score, ")", )
-            ret.append({"title": r["title"], 'score': r.score, 'rank': r.rank})
-
+            ret.append({"title": r["title"], "abstract": r["abstract"], 'score': r.score, 'rank': r.rank})
         # print("\nRun time: " + "{:.5f}".format(results.runtime) + " s")
         return ret
-
 
 def search_something(q_benchmark=""):
     if q_benchmark != "":
         uin = q_benchmark
     else:
-        uin = input("Insert your query: ")  # "An Ethical Foundation for Environmentalism"
+        uin = input("Insert your query: ")
 
-    # print("\nSubmitted OPB query -------")
-    ris_opb = submit_query(uin, OPENB_INDEX_PATH)
+    # print("\nSubmitted query -------")
+    ris = submit_query(uin, INDEX_PATH)
 
-    # print("\nSubmitted SPR query -------")
-    ris_spr = submit_query(uin, SPR_INDEX_PATH)
-
-    total_ris = [j for i in [ris_opb, ris_spr] for j in i]
+    total_ris = [j for i in [ris] for j in i]
     sorted_ris = sorted(total_ris, key=lambda x: x["score"], reverse=True)
 
     print("\n---------------------")
@@ -64,11 +54,14 @@ def search_something(q_benchmark=""):
     print("---------------------")
 
     for w in sorted_ris:
-        print("Book: ", w['title'], " - Score: ", w['score'])
+        print("Paper: ", w['title'])
+        print("Abstract: ", w['abstract'])
+        print("Score: ", w['score'])
         print("---------------------")
 
-    ix = index.open_dir(OPENB_INDEX_PATH)
+    ix = index.open_dir(INDEX_PATH)
     qp = MultifieldParser(["title",
+                           "abstract",
                            "content"],
                           schema=ix.schema,
                           )
