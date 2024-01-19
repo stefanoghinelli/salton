e92db4fd8e5b965c8d9b41c2373fcfb91b62147d
stefanoghinelli <stefano.ghinelli@yahoo.com>
2024-01-19 18:37:19 +0100

    improved query processing

--- src/query_processing.py
+++ src/query_processing.py
@@ -1,51 +1,36 @@
+import re
 from whoosh.qparser import MultifieldParser, OrGroup, AndGroup
-from whoosh import scoring
-from whoosh import index
+from whoosh import scoring, index
 
-INDEX_PATH = "../data/indexes"
+INDEX_PATH = "./data/indexes"
 
-def submit_query(user_query, idx_path):
-    ix = index.open_dir(idx_path)
-    qp = MultifieldParser(["title", 
-                           "abstract",
-                           "content"],  # all selected fields
-                          schema=ix.schema,  # my schema
-                          group=OrGroup  # OR instead AND
-                          )
-    if "AND" in user_query:
-        qp = MultifieldParser(["title",
-                               "abstract",
-                               "content"],  # all selected fields
-                              schema=ix.schema,  # my schema
-                              group=AndGroup  # AND instead OR
-                              )
-
+def submit_query(user_query, searcher, qp):
     user_query = user_query.lower()
     # print("This is uin: " + user_query)
+
+    if re.search(r'\bAND\b', user_query, re.IGNORECASE):
+        qp = MultifieldParser(["title", "abstract", "content"], schema=searcher.schema, group=AndGroup)
+    else:
+        qp = MultifieldParser(["title", "abstract", "content"], schema=searcher.schema, group=OrGroup)
+
     q = qp.parse(user_query)
 
-    # print("This is the parsed query: " + str(q))
-    with ix.searcher(weighting=scoring.BM25F(B=0.75, content_B=1.0, K1=1.2)) as searcher:
-        results = searcher.search(q, limit=5)
-        ret = []
-        # print(str(len(results)) + " results\n")
-        for r in results:
-            # print(r.rank, ".", r["title"], " (Score: ", r.score, ")", )
-            ret.append({"title": r["title"], "abstract": r["abstract"], 'score': r.score, 'rank': r.rank})
-        # print("\nRun time: " + "{:.5f}".format(results.runtime) + " s")
-        return ret
+    results = searcher.search(q, limit=5)
+    ret = []
+    # print(str(len(results)) + " results\n")
+    for r in results:
+        ret.append({"title": r["title"], "abstract": r["abstract"], 'score': r.score, 'rank': r.rank})
+    return ret
 
 def search_something(q_benchmark=""):
-    if q_benchmark != "":
-        uin = q_benchmark
-    else:
-        uin = input("Insert your query: ")
+    ix = index.open_dir(INDEX_PATH)
+    searcher = ix.searcher(weighting=scoring.BM25F(B=0.75, content_B=1.0, K1=1.2))
+    qp = MultifieldParser(["title", "abstract", "content"], schema=searcher.schema, group=OrGroup)
 
-    # print("\nSubmitted query -------")
-    ris = submit_query(uin, INDEX_PATH)
+    uin = q_benchmark if q_benchmark != "" else input("Insert your query: ")
+    ris = submit_query(uin, searcher, qp)
 
-    total_ris = [j for i in [ris] for j in i]
-    sorted_ris = sorted(total_ris, key=lambda x: x["score"], reverse=True)
+    sorted_ris = sorted(ris, key=lambda x: x["score"], reverse=True)
 
     print("\n---------------------")
     print("---------------------")
@@ -54,27 +39,19 @@ def search_something(q_benchmark=""):
     print("---------------------")
 
     for w in sorted_ris:
-        print("Paper: ", w['title'])
-        print("Abstract: ", w['abstract'])
-        print("Score: ", w['score'])
+        print(f"Paper: {w['title']}")
+        print(f"Abstract: {w['abstract']}")
+        print(f"Score: {w['score']}")
         print("---------------------")
 
-    ix = index.open_dir(INDEX_PATH)
-    qp = MultifieldParser(["title",
-                           "abstract",
-                           "content"],
-                          schema=ix.schema,
-                          )
     user_query = uin.lower()
     q = qp.parse(user_query)
-    with ix.searcher(weighting=scoring.BM25F(B=0.75, content_B=1.0, K1=1.2)) as searcher:
-        corrected = searcher.correct_query(q, user_query)
-        if corrected.query != q:
-            print("\n ----> Maybe did you mean:", corrected.string + " ?")
+    corrected = searcher.correct_query(q, user_query)
+    if corrected.query != q and len(sorted_ris) == 0:
+        print(f"\n ----> Maybe did you mean: {corrected.string} ?")
 
-    if q_benchmark != "":
-        return sorted_ris
-
+    searcher.close()
+    return sorted_ris if q_benchmark != "" else None
 
 if __name__ == '__main__':
     search_something()
